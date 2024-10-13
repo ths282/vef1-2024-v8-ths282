6578b4c0e5bfdeb689587d3d761b6326191ef7a1
Ólafur Sverrir Kjartansson <kjarni@gmail.com>
2024-10-13 22:15:10 +0000

    v8

---
+++ scripts.js
@@ -0,0 +1,18 @@
+/**
+ * Sýnilausn á verkefni 8 í Vefforritun 1, 2024.
+ * Byggir á sýnilausn á verkefni 7.
+ * Notar jsdoc til að skrifa lýsingu á föllum, inntaki og úttaki.
+ * Kveikið á `Check JS` í Visual Studio Code til að sjá mögulegar villur.
+ * Notar `console.assert` til að athuga hvort föll virki rétt.
+ */
+
+import { isString, splitOnWhitespace } from './lib/helpers.js';
+
+const test = isString('hæ');
+console.log('test er strengur?', test);
+
+const stringWithWhitespace = `halló
+\theimur
+hæ`;
+const split = splitOnWhitespace(stringWithWhitespace);
+console.log(split);
